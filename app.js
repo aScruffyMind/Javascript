bc1f46a0c9d3343e1c84660b5b8f8c07d771895d
Jason Wilkes <jasonbwilkes@icloud.com>
2021-04-05 23:30:02 -0700

    Refactor: Finalizing refactor
    Refactoring complete, next update will include all comments for code.

--- app.js
+++ app.js
@@ -1,25 +1,50 @@
-let dinosaurArray = []; // remove before turning in
+function convertInches(i) {
+    let feet = Math.floor(i / 12);
+    let inches = i % 12;
+    return `${(feet === 0) ? '' : feet + `'`}${(inches === 0) ? '' : inches + `"`}`;
+}
 
-// FUNCTIONS
+function createGridItem(specimen) {
+    const title = (specimen.species === 'human') ? specimen.name : specimen.species;
+    const grid = document.getElementById('grid');
+    const gridItem = document.createElement('div');
+    const img = document.createElement('img');
+    const titleBox = document.createElement('div');
+    const factBox = document.createElement('div');
+
+    gridItem.classList.add('grid-item');
+    img.setAttribute('src', `images/${(specimen.species).toLowerCase()}.png`);
+    titleBox.classList.add('grid-text', 'name');
+    titleBox.innerHTML = `<h2>${title}</h2>`;
+
+    if (specimen.species != 'human') {
+        factBox.classList.add('grid-text', 'fact');
+        factBox.innerHTML = `<h2>${specimen.displayFact}</h2>`
+        gridItem.appendChild(factBox);
+    }
+    gridItem.appendChild(img);
+    gridItem.appendChild(titleBox);
+    grid.appendChild(gridItem);
+}
+
 const btnClick = (() => { // IIFE declaration to return function for button click event listener
     let readyToSubmit; // initialize the readyToSubmit flag in the closure
-    const inputIDs = (() =>{ // IIFE to return all inputIDs in an array
+    const inputIDs = (() => { // IIFE to return all inputIDs in an array
         const ids = []; // establish empty array to store input IDs
         document.querySelectorAll('input, select').forEach((x) => ids.push(x.id)); // populate ids array from DOM    
         return ids;
-    })(); 
+    })();
     const myForm = document.forms['dino-compare'];
-
-    return function() { // return this function to the variable btnClick
+    return function () { // return this function to the variable btnClick
         readyToSubmit = true; // assume form ok to submit unless this flag becomes false
 
-        inputIDs.forEach((id)=> { // loop to check form for invalid fields before submitting
+        inputIDs.forEach((id) => { // loop to check form for invalid fields before submitting
             const item = document.getElementById(id);
             const required = item.hasAttribute('required');
             if (item.value === '' && required == true) { // check if field is empty and required
                 item.classList.add('field-error'); // add a red border around input field
                 readyToSubmit = false; // mark this form as not ready to submit
-            // else if the item checks out and has the field-error class, remove it.
+                // else if the item checks out and has the field-error class, remove it.
             } else if (item.classList.contains('field-error')) item.classList.remove('field-error');
         });
 
@@ -60,143 +85,85 @@ function Dinos(d) { // units are lbs and inches
     this.fact = d.fact;
 }
 
-Dinos.prototype.compareWeight = function (h) { // compare weight from argument to dino weight.
-    const d = this.weight;
-    const s = this.species;
-    if (h > d) return `You outweigh a ${s} by ${(h - d).toLocaleString()} pounds.`;
-    else if (h < d) return `A ${s} is ${(d - h).toLocaleString()} pounds heavier than you.`;
-    else return `You and a ${s} weigh exactly the same.`;
+Dinos.prototype.compareWeight = function (human) { // compare weight from argument to dino weight.
+    const dino = this.weight;
+    const species = this.species;
+    if (human > dino) return `You outweigh ${species} by ${(human - dino).toLocaleString()} pounds.`;
+    else if (human < dino) return `${species} is ${(dino - human).toLocaleString()} pounds heavier than you.`;
+    else return `You and ${species} weigh exactly the same.`;
 };
 
-Dinos.prototype.compareHeight = function (h) {
-    const d = this.height;
-    const s = this.species;
-    if (h > d) return `You are ${convertInches(h - d)} taller than a ${s}!`;
-    else if (h < d) return `A ${s} is ${convertInches(d - h)} taller than you!`;
-    else return `You are the same height as a ${s}.`;
+Dinos.prototype.compareHeight = function (human) {
+    const dino = this.height;
+    const species = this.species;
+    if (human > dino) return `You are ${convertInches(human - dino)} taller than ${species}.`;
+    else if (human < dino) return `${species} is ${convertInches(dino - human)} taller than you.`;
+    else return `You are the same height as ${species}.`;
 };
 
-Dinos.prototype.compareDiet = function(diet) {
-    const h = diet.toLowerCase();
-    const d = this.diet.toLowerCase();
-    const s = this.species;
-    if (h === d) return `You and ${s} are both ${d}s.`;
-    else return `${s} is ${(d[0] === 'c') ? 'a' : 'an'} ${d}.`;
+Dinos.prototype.compareDiet = function (diet) {
+    const human = diet.toLowerCase();
+    const dino = this.diet.toLowerCase();
+    const species = this.species;
+    if (human === dino) return `You and ${species} are both ${dino}s.`;
+    else return `${species} is ${(dino[0] === 'c') ? 'a' : 'an'} ${dino}.`;
 };
 
-Dinos.prototype.geography = function() {
-    const s = this.species;
-    const w = this.where;
-    switch (w) {
+Dinos.prototype.geography = function () {
+    const species = this.species;
+    const where = this.where;
+    switch (where) {
         case 'North America':
-            return `${s} lived in ${w}`;
+            return `${species} lived in ${where}`;
         case 'North America, Europe, Asia':
-            return `${s} could be found in North America, Europe and Asia.`;
+            return `${species} could be found in North America, Europe and Asia.`;
         case 'World Wide':
-            return `${s} could be found all over the world.`;
+            return `${species} could be found all over the world.`;
     }
 };
 
-Dinos.prototype.period = function() {
+Dinos.prototype.period = function () {
     return `${this.species} lived during the ${this.when} period.`;
 };
 
-
-// function shuffle(array) {
-//     array.sort(() => Math.random() - 0.5);
-// }
-
 function createGrid(human) { // human variable is accessible here
-    const dinosaurs = (function createDinos() {
+    const dinosaurs = (() => {
         let dinoArray = [];
-        let possibleFacts = ['height', 'weight', 'diet', 'where', 'when'];
-
+        let facts = ['height', 'weight', 'diet', 'where', 'when', 'fact', 'random'].sort(() => Math.random() - 0.5);
         for (d = 0; d < data.Dinos.length; d++) {
-            let currentDino = new Dinos(data.Dinos[d]); // add RandomFact here
-
-            currentDino.displayFact = (function() {
+            let currentDino = new Dinos(data.Dinos[d]);
+            currentDino.displayFact = (() => {
                 if (currentDino.species === 'Pigeon') return currentDino.fact;
-                if (Math.floor(Math.random() * 100) < 40) return currentDino.fact;
-                let randomFact;
-                let rando = Math.floor(Math.random() * possibleFacts.length);
-                                
-                switch (rando) {
-                    case 0:
-                        randomFact = currentDino.compareHeight(human.height);
-                        break;
-                    case 1:
-                        randomFact = currentDino.compareWeight(human.weight);
-                        break;
-                    case 2:
-                        randomFact = currentDino.compareDiet(human.diet);
-                        break;
-                    case 3:
-                        randomFact = currentDino.geography();
-                        break;
-                    case 4:
-                        randomFact = currentDino.period();
-                        break;
-                    default: 
-                        randomFact = currentDino.fact;
-                    }
-
-                return randomFact;
-                })();
-                        
+                let x = Math.floor(Math.random() * facts.length);
+                switch (facts[(facts[d] === 'random') ? x : x = d]) {
+                    case 'height':
+                        return currentDino.compareHeight(human.height);
+                    case 'weight':
+                        return currentDino.compareWeight(human.weight);
+                    case 'diet':
+                        return currentDino.compareDiet(human.diet);
+                    case 'where':
+                        return currentDino.geography();
+                    case 'when':
+                        return currentDino.period();
+                    case 'fact':
+                        return currentDino.fact;
+                    default:
+                        return currentDino.fact;
+                }
+            })();
             dinoArray.push(currentDino);
         }
-        dinosaurArray = dinoArray;
-        
-        return dinoArray;
+        return dinoArray.sort(() => Math.random() - 0.5);
     })();
-
-    let dinoOrder = (function () {
-        let numOfDinos = dinosaurs.length;
-        let myArr = [];
-        for (i = 0; i < numOfDinos; i++) {
-            myArr.push(i);
-        };
-        return myArr;
-    })();
-    dinoOrder.sort(() => Math.random() - 0.5);
 
     for (g = 0; g < dinosaurs.length; g++) {
         (g === 4) ? specimen = 'human': specimen = 'dinosaur'
         if (g === 4) {
             createGridItem(human);
-            createGridItem(dinosaurs[dinoOrder[g]]);
-        } else {
-            createGridItem(dinosaurs[dinoOrder[g]]);
-        }
-    }
-}
-
-function createGridItem(specimen) {
-    let title;
-    (specimen.species === 'human') ? title = specimen.name: title = specimen.species;
-    const grid = document.getElementById('grid');
-    const gridItem = document.createElement('div');
-    const img = document.createElement('img');
-    const titleBox = document.createElement('div');
-    const factBox = document.createElement('div');
-    gridItem.classList.add('grid-item');
-    grid.appendChild(gridItem);
-    img.setAttribute('src', `images/${(specimen.species).toLowerCase()}.png`);
-    gridItem.appendChild(img);
-    titleBox.classList.add('grid-text', 'name');
-    titleBox.innerHTML = `<h2>${title}</h2>`;
-    gridItem.appendChild(titleBox);
-    if (specimen.species != 'human') {
-        factBox.classList.add('grid-text', 'fact');
-        factBox.innerHTML = `<h2>${specimen.displayFact}</h2>`
-        gridItem.appendChild(factBox);
+            createGridItem(dinosaurs[g]);
+        } else createGridItem(dinosaurs[g]);
     }
 }
 
 document.getElementById('btn').addEventListener('click', btnClick);
-
-function convertInches(i) {
-    let feet = Math.floor(i / 12);
-    let inches = i % 12;
-    return `${(feet === 0) ? '' : feet + `'`}${(inches === 0) ? '' : inches + `"`}`;
-}
